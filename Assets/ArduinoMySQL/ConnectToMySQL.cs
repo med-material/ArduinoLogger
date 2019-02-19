b2450b4b86c62264c31eb636f76632fdf6dca78c
Bastian Ilsø <bastianilso@gnome.org>
2019-02-19 10:25:41 +0100

    Initial Commit

---
+++ Assets/ArduinoMySQL/ConnectToMySQL.cs
@@ -0,0 +1,419 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System;
+using UnityEngine.Networking;
+using System.Text;
+using UnityEngine.UI;
+
+public class ConnectToMySQL : MonoBehaviour {
+	public static string response = "";
+	public static bool dataReceived = false;
+	public static ConnectToMySQL instance = null;
+
+	private static bool isConnected = false;
+	private int retries = 0;
+	//private Dictionary<string, string> wwwHeader = new Dictionary<string, string> ();
+	private string secHash;
+    private string dataHash;
+	private string colsHash;
+
+	private StreamWriter writer;
+	private string directory;
+	private string fileName;
+	private List<Dictionary<string, List<string>>> logsToUpload;
+	private string sep = ",";
+	private List<string> dataDumps;
+	private List<string> colDumps;
+	
+	[SerializeField]
+	private TextAsset builtInCredentials;
+	private Dictionary<string, string> credentials;
+
+	private float timeout = 5;
+	private float lastUploadTime = -1f;
+	private bool dumplock = false;
+
+	[SerializeField]
+	private Text statusMessage;
+
+	[SerializeField]
+	private Color errorColor;
+	private Color defaultColor;
+
+	private GameObject mysqlCred;
+	private InputField emailInputField;
+	private InputField dbSecKeyInputField;
+	private InputField dbNameField;
+	private InputField tableNameField;
+	private InputField userNameField;
+	private InputField passwordField;
+	private InputField dbURLField;
+	private GameObject connectButton;
+	void Awake() {
+		defaultColor = statusMessage.color;
+		dataDumps = new List<string>();
+		colDumps = new List<string>();
+		credentials = new Dictionary<string, string>();
+		logsToUpload = new List<Dictionary<string, List<string>>>();
+
+		directory = Application.persistentDataPath + "/Data/";
+
+		if(!Directory.Exists(directory)) {
+			Directory.CreateDirectory(directory);
+		}
+
+		if (builtInCredentials == null)  {
+			DetectCredentialsOnDisk();
+		} else {
+			LoadBuiltInCredentials();
+		}
+
+		if (instance == null) {
+			instance = this;
+			if (!isConnected && credentials.Keys.Count > 0) {
+				TestConnectionToServer();
+			}
+		}
+
+		DetectDumpedLogs ();
+	}
+
+	public void TestConnectionToServer () {
+				WWWForm testForm = new WWWForm ();
+				secHash = Md5Sum (credentials["dbSecKey"]);
+				testForm.AddField ("secHashPost", "connectiontest");
+				StartCoroutine(ConnectToServer (testForm));
+	}
+
+	IEnumerator ConnectToServer(WWWForm form) {
+
+		if (credentials["dbURL"] != "") {
+			UnityWebRequest www = UnityWebRequest.Post(credentials["dbURL"], form);
+			//WWW www = new WWW (url, form);
+
+			yield return www.SendWebRequest();
+
+			if(www.isNetworkError || www.isHttpError) {
+				Debug.LogError(www.error);
+				Debug.LogError(www.downloadHandler.text);				
+				yield return new WaitForSeconds(2.0f);
+				retries++;
+				if (retries < 3) {
+					StartCoroutine (ConnectToServer (form));
+					statusMessage.text = "Establishing Connection..".ToUpper();
+					statusMessage.color = defaultColor;
+				} else {
+					statusMessage.text = "DB Error: Could not reach the database.".ToUpper();
+					statusMessage.color = errorColor;
+					connectButton.SetActive(true);
+				}
+			} else {
+				print ("Connected to Server");
+				statusMessage.text = "Connected to Server".ToUpper();
+				statusMessage.color = defaultColor;
+				isConnected = true;
+				SaveCredentialsToDisk();
+				Destroy(mysqlCred);
+			}
+		} else {
+			Debug.LogError("No URL was assigned");
+			connectButton.SetActive(true);
+		}
+	}
+
+	public void AddToUploadQueue(Dictionary<string, List<string>> logCollection) {
+		
+		if (logCollection.Count == 0) {
+			Debug.LogError("logCollection was not initialized with any List<string> columns!");
+			return;
+		}
+
+		if (!logCollection.ContainsKey("Email")) {
+			Debug.LogError("Log Collection does not contain a column with AAU Email Addresses!");
+			return;
+		}
+
+		logsToUpload.Add(new Dictionary<string, List<string>>(logCollection));
+
+		Debug.Log ("Log Collection with " + logCollection.Count + " columns prepared for upload.");
+	}
+
+	public void UploadNow() {
+
+		if (Time.time - lastUploadTime < timeout) {
+			Debug.LogWarning("Don't make frequent calls to MySQL.UploadNow(), uploading is an async operation!");
+		}
+
+		lastUploadTime = Time.time;
+
+		if (logsToUpload.Count == 0) {
+			Debug.LogError("No logs in the upload queue. Use AddToUploadQueue() to add a logCollection to the queue before calling UploadNow().");
+			return;
+		}
+		if (logsToUpload.Count > 0) {
+			foreach (Dictionary<string, List<string>> logCollection in logsToUpload) {
+				Debug.Log ("Attempting to upload logCollection with " + logCollection.Count + " rows.");
+				string dbCols = string.Join(sep,logCollection.Keys);
+				string dataString = ParseDataToString(logCollection);
+				WWWForm form = PrepareForm(dbCols, dataString);
+
+				// dump the data in case submission fails
+				dataDumps.Add(dataString);
+				colDumps.Add(dbCols);
+
+				StartCoroutine (SubmitLogs (form));
+			}
+			logsToUpload.Clear();
+		}
+	}
+	private string ParseDataToString(Dictionary<string, List<string>> logCollection) {
+		// Create a string with the columns
+		string dataString = "";
+		// Create a string with the data
+		for(int i = 0; i < logCollection["Email"].Count; i++) {
+			List<string> row = new List<string>();
+			foreach(string key in logCollection.Keys) {
+				row.Add(logCollection[key][i]);
+			}
+			if(i != 0) {
+				dataString += ";";
+			}
+			dataString += string.Join(sep,row);
+		}
+		return dataString;
+	}
+
+	private WWWForm PrepareForm(string dbCols, string dataString) {
+		WWWForm form = new WWWForm ();
+
+		// Add credentils to form
+		form.AddField ("dbnamePost", credentials["dbName"]);
+		form.AddField ("tablePost", credentials["tableName"]);
+		form.AddField ("usernamePost", credentials["username"]);
+		form.AddField ("passwordPost", credentials["password"]);
+		form.AddField ("secHashPost",Md5Sum (credentials["dbSecKey"]));
+
+		foreach(KeyValuePair<string,string> cred in credentials) {
+		Debug.Log(cred.Key + " = " + cred.Value);
+		}
+
+        colsHash = Md5Sum(dbCols);
+		form.AddField("db_hash", colsHash);
+		Debug.Log ("columns to insert: " + dbCols);
+		form.AddField ("dbCol", dbCols);
+
+        dataHash = Md5Sum(dataString);
+        form.AddField("dataHashPost", dataHash);
+		Debug.Log ("data to submit: " + dataString);
+		form.AddField ("dataPost", dataString);
+		return form;
+	}
+
+	private IEnumerator SubmitLogs(WWWForm form) {
+		
+		Debug.Log ("Submitting logs..");
+		statusMessage.text = "Submitting logs..".ToUpper();
+		statusMessage.color = defaultColor;
+		UnityWebRequest www = UnityWebRequest.Post(credentials["dbURL"], form);
+
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+            Debug.LogError(("Unable to submit logs: " + www.error));
+			statusMessage.text = (www.downloadHandler.text).ToUpper();
+			statusMessage.color = errorColor;
+            Debug.LogError(www.downloadHandler.text);
+			while (dumplock) {
+				yield return new WaitForSeconds(1f);
+			}
+			dumplock = true;
+			DumpLogsToUpload();				
+			dumplock = false;
+        } else {
+			Debug.Log ("Posted successfully");
+			statusMessage.text = "Posted successfully".ToUpper();
+			statusMessage.color = defaultColor;			
+			// Clear datadump structures in case we are submitting dumped data
+			dataDumps.Clear();
+			colDumps.Clear();
+		}
+
+	}
+
+	private void DumpLogsToUpload() {
+		if (dataDumps.Count > 0 && colDumps.Count > 0) {
+			for (int i = 0; i < dataDumps.Count; i++) {
+				var fileDumps = Directory.GetFiles(directory, "logdump*");
+				using (StreamWriter writer = File.AppendText (directory + "logdump" + fileDumps.Length)) {
+					writer.WriteLine (dataDumps[i]);
+				}
+
+				// If a coldump file already exists, we overwrite it.
+				if (File.Exists (directory + "coldump" + fileDumps.Length)) {
+					Debug.LogWarning("Overwriting coldump..");
+					File.Delete (directory + "coldump" + fileDumps.Length);
+				}
+
+				using (StreamWriter writer = File.AppendText (directory + "coldump" + fileDumps.Length)) {
+					writer.WriteLine (colDumps[i]);
+				}
+			}
+			dataDumps.Clear();
+			colDumps.Clear();
+			Debug.Log ("Dumping logdump to disk at: " + directory);
+		}	
+	}
+
+	private void DetectDumpedLogs() {
+
+		var fileDumps = Directory.GetFiles(directory, "logdump*");
+
+		if (fileDumps == null) {
+			return;
+		}
+
+		for (int i = 0; i < fileDumps.Length; i++) {
+			// Remove logdump if we don't have a corresponding coldump file.
+			if (!File.Exists (directory + "coldump" + i)) {
+				Debug.LogWarning("logdump " + i + " was malformed, deleting logdump..");
+				File.Delete (directory + "logdump" + i);
+				continue;
+			}
+
+			string line;
+
+			string dataString = "";
+
+			using (StreamReader reader = new StreamReader(directory + "logdump" + i)) {
+				while((line = reader.ReadLine()) != null)  
+				{  
+					dataString += line;
+				}
+			}
+
+			string dbCols = "";
+
+			using (StreamReader reader = new StreamReader(directory + "coldump" + i)) {
+				while((line = reader.ReadLine()) != null)  
+				{  
+					dbCols += line;
+				}
+			}
+
+			File.Delete (directory + "logdump" + i);
+			File.Delete (directory + "coldump" + i);
+
+			Debug.Log ("Dumped Logs Detected.");
+			
+			// dump the data in case submission fails
+			dataDumps.Add(dataString);
+			colDumps.Add(dbCols);
+
+			WWWForm form = PrepareForm(dbCols, dataString);
+			StartCoroutine (SubmitLogs (form));
+		}
+	}
+
+	private static string Md5Sum(string strToEncrypt) {
+		System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
+		byte[] bytes = ue.GetBytes(strToEncrypt);
+
+		// encrypt bytes
+		System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+		byte[] hashBytes = md5.ComputeHash(bytes);
+
+		// Convert the encrypted bytes back to a string (base 16)
+		string hashString = "";
+
+		for (int i = 0; i < hashBytes.Length; i++)
+		{
+			hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+		}
+
+		return hashString.PadLeft(32, '0');
+	}
+
+	public void SetDatabaseTable(string tableName) {
+		credentials["tableName"] = tableName;
+
+	}
+
+
+	public void Toggle_AuthConnect() {
+		credentials["email"] = emailInputField.text;
+		credentials["dbSecKey"] = dbSecKeyInputField.text;
+		credentials["dbName"] = dbNameField.text;
+		credentials["tableName"] = tableNameField.text;
+		credentials["username"] = userNameField.text;
+		credentials["password"] = passwordField.text;
+		credentials["dbURL"] = dbURLField.text;
+
+		TestConnectionToServer();
+	}
+
+
+	private void SaveCredentialsToDisk() {
+		string authDirectory = Application.persistentDataPath + "/Auth/";
+
+		if (File.Exists (authDirectory + "mysql_auth.txt")) {
+			File.Delete (authDirectory + "mysql_auth.txt");
+		}
+
+		foreach(KeyValuePair<string,string> cred in credentials) {
+			using (StreamWriter writer = File.AppendText (authDirectory + "mysql_auth.txt")) {
+				writer.WriteLine (cred.Key + "=" + cred.Value);
+			}
+		}
+
+		Debug.Log("Credentials saved to: " + authDirectory + "mysql_auth.txt");
+	}
+
+	private void LoadBuiltInCredentials() {
+		string[] cred;
+		string[] lines = builtInCredentials.text.Split('\n');
+		foreach (var line in lines) {
+			if (!string.IsNullOrEmpty(line)) {
+				cred = line.Split('=');
+				credentials[cred[0]] = cred[1];
+			}
+		}
+	}
+
+	private void DetectCredentialsOnDisk() {
+		string authDirectory = Application.persistentDataPath + "/Auth/";
+
+		if (File.Exists (authDirectory + "mysql_auth.txt")) {
+			Debug.Log("Loading credentials from: " + authDirectory + "mysql_auth.txt");
+			string line;
+			string[] cred;
+			using (StreamReader reader = new StreamReader(authDirectory + "mysql_auth.txt")) {
+				while((line = reader.ReadLine()) != null)  
+				{  
+					if (!string.IsNullOrEmpty(line)) {
+						cred = line.Split('=');
+						credentials[cred[0]] = cred[1];
+					}
+				}
+			}
+		} else {
+			mysqlCred = Instantiate(Resources.Load("MySQLAuthCanvas", typeof(GameObject))) as GameObject;
+			mysqlCred.SetActive(true);
+
+			var inputFields = mysqlCred.GetComponentsInChildren<InputField>();
+			emailInputField = inputFields[0];
+			dbSecKeyInputField = inputFields[1];
+			dbNameField = inputFields[2];
+			tableNameField = inputFields[3];
+			userNameField = inputFields[4];
+			passwordField = inputFields[5];
+			dbURLField = inputFields[6];
+
+			var connectButton = GameObject.Find("MySQLConnectButton");
+			Button button = connectButton.GetComponent<Button>();
+			button.onClick.AddListener(() => Toggle_AuthConnect());
+		}
+	}
+		
+}
---
+++ Assets/LogToDisk.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class LogToDisk : MonoBehaviour
+{
+
+    private string filepath = "";
+
+    [SerializeField]
+    private Text filepathText;
+
+	private StreamWriter writer;
+
+    private string directory = "";
+
+    [SerializeField]
+    private GameObject CSVFileLogged;
+
+    void Start()
+    {
+        SetFilePath();
+    }
+
+    public void ResetLoggedState() {
+        CSVFileLogged.SetActive(false);
+    }
+
+    public void SetFilePath(string filename = "", string directoryName = "") {
+
+		if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor) {
+			directory = "C:\\rtii\\" + directoryName + "\\";
+			print ("Windows");
+		}
+		else if(Application.platform == RuntimePlatform.LinuxPlayer || Application.platform == RuntimePlatform.LinuxEditor) {
+			directory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "/rtii/" + directoryName + "/";
+			print("Linux");
+		} else if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer) {
+			directory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "/rtii/" + directoryName + "/";
+			print("Mac OSX");
+		} else {
+            directory = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop) + "/rtii/" + directoryName + "/";
+            print("Unknown");
+        }
+
+        if (string.IsNullOrEmpty(filename)) {
+            filepath = directory + "rtii_output.csv";
+        } else {
+            filepath = directory + filename;
+        }
+        Debug.Log("Filepath: " + filepath);
+        filepathText.text = filepath;
+        
+    }
+
+	public void Log(Dictionary<string, List<string>> logCollection) {
+
+		if(!Directory.Exists(directory)){
+			Directory.CreateDirectory(directory);
+		}
+
+        if (string.IsNullOrEmpty(filepath)) {
+            Debug.LogError("Filepath was not set!");
+        }
+
+		if (File.Exists(filepath)) {
+			Debug.LogWarning("Overwriting CSV file: " + filepath);
+			File.Delete (filepath);
+		}
+
+		string dbCols = string.Join(",",logCollection.Keys);
+
+		using (StreamWriter writer = File.AppendText (filepath)) {
+			writer.WriteLine (dbCols);
+		}
+
+		List<string> dataString = new List<string>();
+		// Create a string with the data
+		for(int i = 0; i < logCollection["Email"].Count; i++) {
+			List<string> row = new List<string>();
+			foreach(string key in logCollection.Keys) {
+				row.Add(logCollection[key][i]);
+			}
+			dataString.Add(string.Join(",",row) + ";");
+		}
+		
+		foreach (var log in dataString) {
+			using (StreamWriter writer = File.AppendText (filepath)) {
+				writer.WriteLine (log);
+			}
+		}
+
+		Debug.Log("Data logged to: " + filepath);
+        CSVFileLogged.SetActive(true);
+	}
+
+}
