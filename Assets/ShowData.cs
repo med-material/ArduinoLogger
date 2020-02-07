ea636f1c050f0d523f45cb99408287b65a14660a
Bastian Ilso <contact@bastianilso.com>
2020-02-07 15:21:47 +0100

    ShowData: Open URL when logs are uploaded
    ShowData is a class which handles showing visualizations
    after the logs has been published to the server.
    It only does so if based on selected arduino
    firmware IDs that it recognizes.

---
+++ Assets/ShowData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowData : MonoBehaviour
+{
+    
+    string webapplication = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetTargetDatabaseTable(string tablename) {
+        if (tablename == "reactiontime" || tablename == "synch") {
+            webapplication = "http://create-rapps01.srv.aau.dk/reaction-synch-tests/";
+        } 
+    }
+
+    public void ShowRShinyWebApplication(LogUploadResult logUploadResult) {
+        if (logUploadResult.status == LogUploadStatus.Success) {
+            Application.OpenURL(webapplication);
+        }
+    }
+}
