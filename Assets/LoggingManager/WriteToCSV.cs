74f3220c37a73cea05ddbd51e39158cf42ccc015
Vincent <vincentsd@yahoo.com>
2021-12-07 10:11:00 +0100

    Implementing the WriteToCsv class
    This class will allow to save the logs in a file.
    Each LogToCSV object is instantiated with a file path and a LogStore object.
    When calling the WriteAll() function, the logs are exported from the LogStore object as a string and are written in the file at the given file path.
    The process is currently executed in a separated thread to prevent the main application from freezing.

---
+++ Assets/LoggingManager/WriteToCSV.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+
+public class WriteToCSV
+{
+    private string fileName;
+    private string savePath;
+    private string filePath;
+
+    private LogStore logStore;
+
+
+    public WriteToCSV(LogStore logStore, string savePath, string filePrefix, string fileExtension)
+    {
+        this.fileName = filePrefix + "_" +
+                        DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss_ffff") + "_" + logStore.Label + fileExtension;
+        Init(logStore, savePath);
+    }
+
+    public WriteToCSV(LogStore logStore, string savePath, string fileName)
+    {
+        this.fileName = fileName;
+        Init(logStore, savePath);
+    }
+
+    public WriteToCSV(LogStore logStore, string filePath)
+    {
+        this.filePath = filePath;
+        this.logStore = logStore;
+    }
+
+    private void Init(LogStore logStore, string path)
+    {
+        this.savePath = path == "" ? Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) : path;
+        this.filePath = Path.Combine(savePath, fileName);
+        this.logStore = logStore;
+    }
+
+    //Writes all the logs into the file at the given filepath
+    public void WriteAll(Action callback)
+    {
+        //This part has to be executed in the main Thread 
+        if (logStore.CurrentLogRow.Count != 0)
+        {
+            logStore.EndRow();
+        }
+
+        new Thread(() =>
+        {
+            //disables the possibility to log data durring the saving process
+            logStore.IsReadOnly = true;
+            Write();
+            logStore.IsReadOnly = false;
+            callback();
+        }).Start();
+    }
+
+
+
+    private void Write()
+    {
+        Stopwatch exportToStringStopwatch = new Stopwatch();
+        exportToStringStopwatch.Start();
+
+        string dataString = logStore.ExportAll<string>();
+
+        exportToStringStopwatch.Stop();
+        TimeSpan exportToStringTs = exportToStringStopwatch.Elapsed;
+        string exportToStringElapsedTime = String.Format("{0:00}:{1:0000}",
+            exportToStringTs.Seconds, exportToStringTs.Milliseconds);
+        Debug.Log(logStore.Label + " string exported in " + exportToStringElapsedTime);
+
+        string headers = logStore.GenerateHeaders();
+
+        Stopwatch writeStopwatch = new Stopwatch();
+        writeStopwatch.Start();
+
+        using (var file = new StreamWriter(filePath, true))
+        {
+            file.WriteLine(headers);
+            file.Write(dataString);
+        }
+
+        writeStopwatch.Stop();
+        TimeSpan writeTs = writeStopwatch.Elapsed;
+        string writeElapsedTime = String.Format("{0:00}:{1:0000}",
+            writeTs.Seconds, writeTs.Milliseconds);
+        Debug.Log(logStore.Label + " logs wrote to file in " + writeElapsedTime);
+    }
+
+}
